03e70515e51728599f7c24ccd985a417b76a799b
K0IN <thisk0in@gmail.com>
2024-03-27 00:55:59 +0100

    Remove build artefacts

--- llm/generate/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/e/src/ollama-2/llm/ext_server
-# Build directory: /mnt/e/src/ollama-2/llm/generate/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("_deps/llama-build")
--- llm/generate/build/_deps/llama-build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/e/src/ollama-2/llm/generate/build/_deps/llama-src
-# Build directory: /mnt/e/src/ollama-2/llm/generate/build/_deps/llama-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
